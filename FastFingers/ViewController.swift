fb9ec6bbff72237205c942224575264a2a69a382
Ludvig Løite <Ludvig@ludvigs-macbook-pro.local>
2018-10-21 15:53:14 +0200

    Initial Commit

---
+++ FastFingers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastFingers
+//
+//  Created by Ludvig Løite on 21/10/2018.
+//  Copyright © 2018 Ludvig Løite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
